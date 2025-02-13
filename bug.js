3ebc8549d351fb05fa87c08ec547d5e30126e83a
BugHunter-X <askzenai@gmail.com>
2025-02-13 03:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080); //This will cause an error if port 8080 is already in use
+console.log('Server is running on port 8080');
